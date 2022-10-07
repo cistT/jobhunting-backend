268174dbb19f12623d2efc58e76ea331b1461bd2
cistT <cist.b220.t.chitose@gmail.com>
2022-10-07 16:50:18 +0900

    アプリの実行をする関数を定義

---
+++ src/main/kotlin/com/jobhunting/JobhuntingApplication.kt
@@ -0,0 +1,12 @@
+package com.jobhunting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobhuntingApplication
+
+
+fun main(args: Array<String>) {
+	runApplication<JobhuntingApplication>(*args)
+}
