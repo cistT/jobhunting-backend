def5c9cf275e63e9a8460babc497b1d1e8312409
cistT <cist.b220.t.chitose@gmail.com>
2022-10-10 10:10:30 +0900

    deleteメソッドで削除処理をするように変更

--- src/main/kotlin/com/jobhunting/company/Controller.kt
+++ src/main/kotlin/com/jobhunting/company/Controller.kt
@@ -1,32 +1,42 @@
 package com.jobhunting.company
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"])
 class Controller(val jdbcTemplate: JdbcTemplate) {
 
     @GetMapping("/company/userID={userID}")
     fun getRegisteredCompany(
         @PathVariable("userID") userID: String
     ):CompanyData {
-        return CompanyData(companyData=Model(jdbcTemplate).getCompanyData(userID))
+        return   CompanyData(companyData = Model(jdbcTemplate).getCompanyData(userID))
     }
 
-    @PostMapping("/company/register/userID={userID}")
+
+    @PostMapping("/company/register/userID={userID}",
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
     fun registerCompany(
         @PathVariable("userID") userID: String,
-        @RequestBody request:Company
+        @RequestBody @Validated request:Company
     ){
+
         Model(jdbcTemplate).registerCompanyData(userID,request)
     }
 
-    @PostMapping("/company/delete/userID={userID}")
+    @DeleteMapping("/company/delete/userID={userID}",consumes = ["application/json"])
+    @ResponseStatus(code=HttpStatus.OK)
     fun deleteCompany(
         @PathVariable("userID") userID: String,
         @RequestBody request: CompanyID
-    ){
+    ):ResponseMessage{
         Model(jdbcTemplate).deleteRegisteredCompany(userID,request.id)
+        return ResponseMessage(200,HttpStatus.OK)
     }
 
 }
